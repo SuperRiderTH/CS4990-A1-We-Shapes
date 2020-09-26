749c1ee192b4f86160ba5f829b687b1c2c7e66f0
Sean <SeanSrockOHS@gmail.com>
2020-09-26 06:46:54 -0400

    Implement collectable counter HUD.

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -8,12 +8,22 @@ public class HUDController : MonoBehaviour
 
     Text textTime;
     GameControllerScript gameController;
+    public GameObject collectableImage;
 
     // Start is called before the first frame update
     void Start()
     {
         textTime = GameObject.Find("TimeText").GetComponent<Text>();
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControllerScript>();
+
+        for (int i = 0; i < gameController.collectablesAvailable; i++)
+        {
+            GameObject temp = Instantiate(collectableImage);
+            temp.transform.SetParent (GameObject.FindGameObjectWithTag("HUD").transform, false);
+            temp.GetComponent<IconController>().index = i;
+            temp.GetComponent<RectTransform>().anchoredPosition = new Vector3( (40 + ( 73 * (i) ) ), -100, 0 ) ;
+        }
+
     }
 
     // Update is called once per frame
