855ee77caca452713328ac334a1938291ea1234c
Sean <SeanSrockOHS@gmail.com>
2020-10-08 01:24:53 -0400

    Rotate the shape to face movement direction. Don't allow the shape to move when not on ground.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
 
     public bool playerInControl = true;
 
-    private float speed = 12f;
+    private float speed = 5f;
 
 
     // Start is called before the first frame update
@@ -50,6 +50,35 @@ public class PlayerController : MonoBehaviour
         // If this isn't the controlled object, stop.
         if ( !playerInControl ) { return; }
 
+        bool _ground = false;
+
+        // A bunch of raycasts to see if the ground exists.
+        bool _groundCenter = Physics.Raycast(playerRigidbody.position, Vector3.down, 2);
+
+        bool _groundForward = Physics.Raycast(playerRigidbody.position + Vector3.forward + Vector3.left, Vector3.down, 1);
+        bool _groundBackward = Physics.Raycast(playerRigidbody.position + Vector3.forward + Vector3.right, Vector3.down, 1);
+        bool _groundLeft = Physics.Raycast(playerRigidbody.position + Vector3.back + Vector3.left, Vector3.down, 1);
+        bool _groundRight = Physics.Raycast(playerRigidbody.position + Vector3.back + Vector3.right, Vector3.down, 1);
+
+        // Draw them in the scene view.
+        Debug.DrawRay(playerRigidbody.position, Vector3.down, Color.red);
+        Debug.DrawRay(playerRigidbody.position + Vector3.forward + Vector3.left, Vector3.down, Color.red);
+        Debug.DrawRay(playerRigidbody.position + Vector3.forward + Vector3.right, Vector3.down, Color.red);
+        Debug.DrawRay(playerRigidbody.position + Vector3.back + Vector3.left, Vector3.down, Color.red);
+        Debug.DrawRay(playerRigidbody.position + Vector3.back + Vector3.right, Vector3.down, Color.red);
+
+        if (!_groundCenter && !_groundForward && !_groundBackward && !_groundLeft && !_groundRight)
+        {
+            _ground = false;
+        }
+        else
+        {
+            _ground = true;
+        }
+
+        // Prevent player movement if you are not on the ground.
+        if (!_ground) { return; }
+
         // Get the input of the player.
         float _inputH = Input.GetAxis("Horizontal");
         float _inputV = Input.GetAxis("Vertical");
@@ -62,16 +91,30 @@ public class PlayerController : MonoBehaviour
         // Create the final rotation from what we have.
         Vector3 _finalRotation = Quaternion.Euler(0, _cameraAngle, 0) * _inputs;
 
-        // Move the rigidbody.
-        playerRigidbody.MovePosition(transform.position + ( _finalRotation * speed * Time.deltaTime ) );
+        // If we don't want to go anywhere, stop running code.
+        if (_finalRotation == Vector3.zero) { return; }
 
-        // NOTE: This will not rotate the player object, this will be implemented later.
+        Vector3 _targetPosition = playerRigidbody.position + (_finalRotation * speed * Time.deltaTime);
+
+        // We get the temporary locations so we can point the shape.
+        var _tempLoc = playerRigidbody.position;
+
+        // Move the rigidbody.
+        playerRigidbody.MovePosition(_targetPosition);
+
+        var _tempLoc2 = playerRigidbody.position;
+
+        // Figure out where we are going, and rotate to face there.
+        var _targetDirection = _tempLoc2 - _tempLoc;
+        var _targetRotation = Vector3.RotateTowards(transform.forward, _targetDirection, speed * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(_targetRotation);
 
     }
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         PlayerMovement();
