6e97daf5fe1983069ea756d6837e5852bb783db5
Wade Falk <71107065+Wolfgang-Falk@users.noreply.github.com>
2020-10-08 23:36:55 -0500

    Update VerticalMovementDown.cs
    Wade: Updates per 10/8/2020

--- Assets/Scripts/Platform Movement/VerticalMovementDown.cs
+++ Assets/Scripts/Platform Movement/VerticalMovementDown.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,28 +8,45 @@ public class VerticalMovementDown : MonoBehaviour
     private Vector3 pos1;
     private Vector3 pos2;
 
-
+	// This determines if triggered by collectible (Wade)
+	public bool requiresCollectible = false;
+	public GameObject collectible;
+	
+	public bool pauseAtEnd = false;			// This prevents PingPong (Wade)
+	public float downValue = 10.0f;			// Edit value for pos2 (Wade)
     public float speed = 1f;
 
     private void OnTriggerStay(Collider other)
     {
         other.transform.SetParent(transform);
-    }
+    } // end OnTriggerStay
+
     private void OnTriggerExit(Collider other)
     {
         other.transform.SetParent(null);
-    }
+    } // end OnTriggerExit
+
     // Start is called before the first frame update
     private void Start()
     {
-        pos1 = transform.position;
-        pos2 = pos1;
-        pos2.y = pos1.y - 10.0f;
-    }
+		pos1 = transform.position;
+		pos2 = pos1;
+		pos2.y = pos1.y - downValue;
+    } // end Start
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
-    }
-}
+		if ((requiresCollectible == true && collectible == null) ||
+			(requiresCollectible == false)) {
+
+			if (pauseAtEnd == false) {
+				transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong (Time.time * speed, 1.0f));
+			} else {
+				while (transform.position.y > pos2.y) {
+					transform.position = Vector3.Lerp (pos1, pos2, speed);
+				} // end while
+			} // end if
+		} // end if
+	} // end Update
+} // end VerticalMovementDown
